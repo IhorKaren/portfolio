ff66dde33a3a6ab07141e235f69d4335c7ea38f4
Ihor Karen <karenihor@gmail.com>
2023-12-28 10:30:40 +0200

    comments

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,14 @@
 import {
   SiHtml5,
   SiSass,
-  SiTailwindcss,
+  // SiTailwindcss,
   SiMui,
   SiJavascript,
   SiTypescript,
   SiReact,
   SiNextdotjs,
   SiNodedotjs,
-  SiNestjs,
+  // SiNestjs,
 } from 'react-icons/si';
 
 import MenuTitle from 'components/MenuTitle/MenuTitle';
@@ -17,14 +17,14 @@ import { Form, Label, Input } from './Filter.styled';
 const variants = [
   { id: 1, name: 'HTML', icon: SiHtml5 },
   { id: 2, name: 'SASS', icon: SiSass },
-  { id: 3, name: 'Tailwind', icon: SiTailwindcss },
+  // { id: 3, name: 'Tailwind', icon: SiTailwindcss },
   { id: 4, name: 'MaterialUI', icon: SiMui },
   { id: 5, name: 'JavaScript', icon: SiJavascript },
   { id: 6, name: 'TypeScript', icon: SiTypescript },
   { id: 7, name: 'React', icon: SiReact },
   { id: 8, name: 'Next.js', icon: SiNextdotjs },
   { id: 9, name: 'Node.js', icon: SiNodedotjs },
-  { id: 10, name: 'NestJS', icon: SiNestjs },
+  // { id: 10, name: 'NestJS', icon: SiNestjs },
 ];
 
 const Filter = ({ onChange }) => {
